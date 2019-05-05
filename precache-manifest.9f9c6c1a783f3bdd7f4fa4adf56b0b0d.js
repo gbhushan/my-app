d658c60fdea6da4effdae84654670590dade5763
gaurav <gaurav@lumity.com>
2019-05-04 17:31:27 -0700

    Updates

---
+++ precache-manifest.9f9c6c1a783f3bdd7f4fa4adf56b0b0d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "004a391d361869709cf40e7baa576299",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "e44ca398a3064c7767db",
+    "url": "/my-app/static/css/2.6466deae.chunk.css"
+  },
+  {
+    "revision": "d3c53e740488fb36f531",
+    "url": "/my-app/static/css/main.597a65fa.chunk.css"
+  },
+  {
+    "revision": "e44ca398a3064c7767db",
+    "url": "/my-app/static/js/2.ef0a232a.chunk.js"
+  },
+  {
+    "revision": "d3c53e740488fb36f531",
+    "url": "/my-app/static/js/main.cbe8d9ac.chunk.js"
+  },
+  {
+    "revision": "9930044c31793394defe",
+    "url": "/my-app/static/js/runtime~main.6019dd84.js"
+  }
+]);
